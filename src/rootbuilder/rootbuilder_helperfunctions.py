eb7618fb411f34133f00cdad915f69d62e0e4738
Vaneixus <54147992+Vaneixus@users.noreply.github.com>
2021-04-17 23:02:01 -0400

    Update rootbuilder_helperfunctions.py

--- src/rootbuilder/rootbuilder_helperfunctions.py
+++ src/rootbuilder/rootbuilder_helperfunctions.py
@@ -4,7 +4,7 @@
 #                                                                             #
 # This file is licensed under the terms and conditions of the MIT License.    #
 # Please see the accompanying file named LICENSE_MIT for the full license,    #
-# if the file is missing or damaged, lease see <https://mit-license.org/> for #
+# if the file is missing or damaged, please see <https://mit-license.org/>    #
 # for the full MIT license.                                                   #
 #                                                                             #
 ###############################################################################
