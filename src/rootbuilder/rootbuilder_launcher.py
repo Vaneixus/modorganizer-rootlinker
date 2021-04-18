2d83bee34cea3b2c2d02cd91e055bb56a5d811f5
Vaneixus <54147992+Vaneixus@users.noreply.github.com>
2021-04-17 22:56:20 -0400

    Code Seperation
    Current Build transformed into a multi-file python package. Licensing changes were done to reflect new structure and allow more freedoms.

---
+++ src/rootbuilder/rootbuilder_launcher.py
@@ -0,0 +1,95 @@
+###############################################################################
+#                                                                             #
+#                  Copyright(C) 2020 Vaneixus Prime                           #
+#                                                                             #
+#  Root Builder is free software: you can redistribute it and/or modify       #
+#  it under the terms of the GNU General Public License as published by       #
+#  the Free Software Foundation, either version 3 of the License, or          #
+#  any later version.                                                         #
+#                                                                             #
+#  Root Builder is distributed in the hope that it will be useful, but        #
+#  WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY  #
+#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License   #
+#  for more details.                                                          #
+#                                                                             #
+#  You should have received a copy of the GNU General Public License          #
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+
+from PyQt5.QtCore import QCoreApplication
+import mobase
+
+from . import rootbuilder_helperfunctions as _helperf
+from . import rootbuilder_usvfslibrary as _usvfslib
+# from . import rootbuilder_linklibrary as linklib
+
+class RootBuilder(mobase.IPluginFileMapper):
+
+    ############################
+    ## IPlugin Data Structure ##
+    ############################
+
+    def __init__(self):
+        super(RootBuilder, self).__init__()
+
+    def init(self, organizer):
+        # Initialise variables
+        self.iOrganizer = organizer
+        self.helperf = _helperf.helperf(organizer)
+        self.usvfslib = _usvfslib.RootBuilderUSVFSLibrary(organizer)
+        return True
+
+    def name(self):
+        return "Root Builder"
+
+    def author(self):
+        return "Vaneixus Prime"
+
+    def description(self):
+        return self.__tr("Adds support for root-Level game modding.")
+
+    def version(self):
+        return mobase.VersionInfo(2, 0, 0, 1, mobase.ReleaseType.prealpha)
+
+    def settings(self):
+        return [mobase.PluginSetting(
+            "enabled",
+            self.__tr("Enable Plugin"),
+            True),
+            mobase.PluginSetting(
+            "link_enabled",
+            self.__tr("Enable Linking Files"),
+            True),
+            mobase.PluginSetting(
+            "link_extensions",
+            self.__tr("a list of extensions to be linked."),
+            True),
+            mobase.PluginSetting(
+            "ow_cleanup",
+            self.__tr("Clean up empty/useless folders and files from"
+                      + " the overwrite folder."),
+            True)
+        ]
+
+    def isActive(self):
+        return self.iOrganizer.pluginSetting(self.name(), "enabled")
+
+    def __tr(self, trstr):
+        return QCoreApplication.translate("RootBuilder", trstr)
+
+    ######################################
+    ## IPluginFileMapper Data Structure ##
+    ######################################
+
+    def mappings(self):
+        self.mappedFiles = []
+        rootOverwriteMapping = mobase.Mapping()
+        rootOverwriteMapping.source = str(self.helperf.rootOverwritePath())
+        rootOverwriteMapping.destination = str(self.helperf.gamePath())
+        rootOverwriteMapping.isDirectory = True
+        rootOverwriteMapping.createTarget = True
+        self.mappedFiles = self.usvfslib.mountRootModsDirs()
+        self.mappedFiles.append(rootOverwriteMapping)
+        return self.mappedFiles
---
+++ src/rootbuilder/rootbuilder_usvfslibrary.py
@@ -0,0 +1,135 @@
+###############################################################################
+#                                                                             #
+#                  Copyright(C) 2020 Vaneixus Prime                           #
+#                                                                             #
+#  Root Builder USVFS Library is free software: you can redistribute it       #
+#  and/or modify it under the terms of the GNU Lesser General Public License  #
+#  as published by the Free Software Foundation, either version 3 of the      #
+#  License, or any later version.                                             #
+#                                                                             #
+#  Root Builder USVFS Library is distributed in the hope that it will be      #
+#  useful, but WITHOUT ANY WARRANTY without even the implied warranty of      #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU   #
+#  General Public License for more details.                                   #
+#                                                                             #
+#  You should have received a copy of the Lesser GNU General Public License   #
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+
+from PyQt5.QtCore import QCoreApplication
+from pathlib import Path
+import os
+
+import mobase
+from . import rootbuilder_helperfunctions as _helperf
+
+class RootBuilderUSVFSLibrary():
+
+    def __init__(self, organizer):
+        self.iOrganizer = organizer
+        self.helperf = _helperf.helperf(organizer)
+        super(RootBuilderUSVFSLibrary, self).__init__()
+
+    ##################################
+    ## File Mapper Plugin Structure ##
+    ##################################
+
+    ###
+    # @Summary: Called by MO2 when starting programs to get what to re-route.
+    #           Prepares root game folder mod mappings and sets up the
+    #            overwrite/Root creation target folder.
+    # @Returns: A Mapping Object list.(Mapping Object list)
+    ###
+
+    def mappings(self):
+        self.mappedFiles = []
+        rootOverwriteMapping = mobase.Mapping()
+        rootOverwriteMapping.source = str(self.helperf.rootOverwritePath())
+        rootOverwriteMapping.destination = str(self.helperf.gamePath())
+        rootOverwriteMapping.isDirectory = True
+        rootOverwriteMapping.createTarget = True
+        self.mountRootModsDirs()
+        self.mappedFiles.append(rootOverwriteMapping)
+        return self.mappedFiles
+
+    #############################
+    ## Custom Plugin Structure ##
+    #############################
+
+    iOrganizer = None
+
+    mappedFiles = []
+
+    ###
+    # @Return: get list of all (mod)/Root folders, skipping (mod)/Root/Data cases.
+    #         (Strings List)
+    ###
+    def getRootMods(self):
+        modslist = self.iOrganizer.modList().allModsByProfilePriority()
+        rootMods = []
+        for modName in modslist:
+            if (self.iOrganizer.modList().state(modName) &
+                    mobase.ModState.active):
+                if (self.helperf.modsPath() / modName / "Root").exists():
+                    if not (self.helperf.modsPath() / modName
+                            / "Root" / "Data").exists():
+                        #qDebug("RootBuilder: /Root detected, adding mod(" 
+                        #       + modName + ") to root mapping.")
+                        rootMods.append(modName)
+                    #else:
+                        #qDebug(
+                        #    "RootBuilder: Root/Data detected, skipping: " +
+                        #    modName + ".")
+        return rootMods
+
+    ###
+    # @Summary: Mounts the files
+    ###
+    def mountRootModsDirs(self):
+        # Cleanup root overwrite directory
+        if self.helperf.rootOverwritePath().exists():
+            self.cleanupOverwriteFolder()
+        else:
+            os.mkdir(self.helperf.rootOverwritePath())
+        #qDebug("RootBuilder: About to mount Root mods")
+        modsNameList = self.getRootMods()
+        self.usvfsReroute(modsNameList)
+        return self.mappedFiles
+
+    ###
+    # @Summary: Re-route files using USVFS
+    # @Parameter: Active mods' name list.(String List)
+    ###
+    def usvfsReroute(self, modsNameList):
+        #qDebug("Root Builder: Mounting using USVFS")
+        for modName in modsNameList:
+        #    qDebug("Root Builder: Re-routing (\""
+        #           + str(self.helperf.modsPath() / modName / "Root")
+        #           + "\") To (\"" + str(self.helperf.gamePath()) + "\")")
+            rootMapping = mobase.Mapping()
+            rootMapping.source = str(self.helperf.modsPath() / modName
+                                     / "Root")
+            rootMapping.destination = str(self.helperf.gamePath())
+            rootMapping.isDirectory = True
+            rootMapping.createTarget = False
+            self.mappedFiles.append(rootMapping)
+
+    ###
+    # @Summary: Cleans up the overwrite folder from useless files/folders.
+    ###
+    def cleanupOverwriteFolder(self):
+        if not self.iOrganizer.pluginSetting("Root Builder",
+                                             "ow_cleanup"):
+            return
+        print("RootBuilder: Cleaning up root overwrite folder...")
+        # Delete root overwrite folder in case it's empty
+        if not self.helperf.rootOverwritePath().exists():
+            return
+        if len(os.listdir(self.helperf.rootOverwritePath())) == 0:
+            print("RootBuilder: cleaning up empty overwrite/Root folder")
+            os.rmdir(self.helperf.rootOverwritePath())
+        else:
+            print("RootBuilder: there are files in overwrite/Root, no cleanup")
+        return
