29b581ef8b8ecdc9a673cf2c930fce6bda4f1105
Vaneixus <54147992+Vaneixus@users.noreply.github.com>
2021-04-17 23:00:14 -0400

    Fixed File License Notice
    Modified License Notice to better represent the current file's usage terms and conditions.

--- src/rootbuilder/rootbuilder_helperfunctions.py
+++ src/rootbuilder/rootbuilder_helperfunctions.py
@@ -2,10 +2,10 @@
 #                                                                             #
 #                 Copyright(C) 2021 Vaneixus Prime                            #
 #                                                                             #
-# Root Builder Link Library is a derieved work and is licensed under the      #
-# terms and conditions of the MIT License. Please see the accompanying file   #
-# named LICENSE_MIT for the full license, if the file is missing or damaged,  #
-# please see <https://mit-license.org/> for the full MIT license.             #
+# This Program is licensed under the terms and conditions of the MIT License. #
+# Please see the accompanying file named LICENSE_MIT for the full license,    #
+# if the file is missing or damaged, lease see <https://mit-license.org/> for #
+# for the full MIT license.                                                   #
 #                                                                             #
 ###############################################################################
    
