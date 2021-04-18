ad2d60a2841f424fba071f1c6d56c0543248a2b3
Vaneixus <54147992+Vaneixus@users.noreply.github.com>
2021-04-17 22:51:36 -0400

    Successful Code Seperation
    Seperated the original Root Builder build into three different files

---
+++ rootbuilder/rootbuilder_linklibrary.py
@@ -0,0 +1,10 @@
+###############################################################################
+#                                                                             #
+#                 Copyright(C) 2021 Deorder & Vaneixus Prime                  #
+#                                                                             #
+# Root Builder Link Library is a derieved work and is licensed under the      #
+# terms and conditions of the MIT License. Please see the accompanying file   #
+# named LICENSE_MIT for the full license, if the file is missing or damaged,  #
+# please see <https://mit-license.org/> for the full MIT license.             #
+#                                                                             #
+###############################################################################
